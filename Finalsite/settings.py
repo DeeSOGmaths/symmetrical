631f1c0d3cf89c535cf7b477005c80b07526216e
DeeSOGmaths <deeboytogun@gmail.com>
2025-05-22 17:55:56 +0100

    second commit

--- Finalsite/settings.py
+++ Finalsite/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Added this line to include WhiteNoise middleware
 ]
 
 ROOT_URLCONF = 'Finalsite.urls'
